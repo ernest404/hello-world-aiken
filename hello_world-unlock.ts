6f30ecbaba83ab104ef417f4560351fb6f24b432
ernest404 <ernestmuisyoo@gmail.com>
2023-01-31 15:05:21 +0300

    added lock and unlock transactions

--- hello_world-unlock.ts
+++ hello_world-unlock.ts
@@ -15,7 +15,7 @@ import {
   const lucid = await Lucid.new(
     new Blockfrost(
       "https://cardano-preview.blockfrost.io/api/v0",
-      /* BLOCKFROST API KEY */,
+      "previewxplsRlSK9N2QlfSsZ9wl3yib3inYGCxu",
     ),
     "Preview",
   );
@@ -33,4 +33,36 @@ import {
     };
   }
 
-  
+  // ^^^ Code above is unchanged unchanged. ^^^
+ 
+const utxo = { txHash: "cdc742dbd09db6937739488b48fab11bf56df3da7bcbf71396bda6fb44cc7800", outputIndex: 0 };
+ 
+const redeemer = Data.to(new Constr(0, [utf8ToHex("Hello, World!")]));
+ 
+const txUnlock = await unlock(utxo, { from: validator, using: redeemer });
+ 
+await lucid.awaitTx(txUnlock);
+ 
+console.log(`1 ADA recovered from the contract
+    Tx ID: ${txUnlock}
+    Redeemer: ${redeemer}
+`);
+ 
+// --- Supporting functions
+ 
+async function unlock(ref, { from, using }): Promise<TxHash> {
+  const [utxo] = await lucid.utxosByOutRef([ref]);
+ 
+  const tx = await lucid
+    .newTx()
+    .collectFrom([utxo], using)
+    .addSigner(await lucid.wallet.address())
+    .attachSpendingValidator(from)
+    .complete();
+ 
+  const signedTx = await tx
+    .sign()
+    .complete();
+ 
+  return signedTx.submit();
+}
